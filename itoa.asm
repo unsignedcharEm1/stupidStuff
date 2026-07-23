3069c14f945d0d7b5e07e92cb1b22b72c0b5e13b
UnsignedChar <unsignedchar.email@gmail.com>
2026-07-23 14:15:16 +0600

    itoa.asm: comment it to make it friendly
    I swear to god this piece of god has sparked a debate in my own University group

--- itoa.asm
+++ itoa.asm
@@ -1,25 +1,25 @@
-global _start
-section .bss
-	buf resb 20
+global _start ; declare to the linker that the _start symbol exists
+section .bss 
+	buf resb 20 ; reserve 20 bytes at runtime 
 section .text
 _start:
-	mov rax, 67676767
-	call print_udiv
+	mov rax, 67676767 ; we are gonna put something into rax register to print
+	call print_udiv 
 	jmp exit
 print_udiv:
-	lea rsi, [buf+19]
-	mov byte [rsi], 0x0a
-	mov rbx, 10
+	lea rsi, [buf+19] ; go to the end of the buffer
+	mov byte [rsi], 0x0a ; and put a '\n'
+	mov rbx, 10 ; now we are gonna convert the value in rax into ascii so we prepare to divide by 10
 .next:
-	dec rsi
-	xor rdx, rdx
-	div rbx
-	add rdx, '0'
-	mov byte [rsi], dl
-	test rax, rax
-	jnz .next
-	lea rdx, [buf+20]
-	sub rdx, rsi
+	dec rsi ; move the pointer to the buffer to the left by 1
+	xor rdx, rdx ; clear rdx into 0
+	div rbx ; divide rax by rbx and the remainder is stored in rdx
+	add rdx, '0' ; now we just add the ascii value of '0' to convert it into ascii
+	mov byte [rsi], dl ; since ascii doesnt go over 8 bit we just use the 8 bit version of the register and put it in the buffer
+	test rax, rax ; check if rax is equal to 0 by doing an AND operation on it self
+	jnz .next ; if our test statement is not zero then repeat this whole process
+	lea rdx, [buf+20] ; now we are gonna calculate the length of our string by going to the end of buf + 1 and then
+	sub rdx, rsi ; since rsi has reached the start of the buffer we can just subtract it from rdx
 	; print
 	mov rax, 1
 	mov rdi, 1
